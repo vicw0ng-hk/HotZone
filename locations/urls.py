22d9deadad0b01f0981e4c4c57db8431511cba25
vicw0ng-hk <62649152+vicw0ng-hk@users.noreply.github.com>
2020-11-09 18:46:18 +0800

    restructure

--- locations/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'locations'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('select/<str:query>', views.select, name='select'),
-]
