71a8055d4e9eff60505ec8a9592fff56df79bb11
Victor <62649152+vicw0ng-hk@users.noreply.github.com>
2020-12-01 13:01:21 +0800

    Revert "Merge pull request #8 from vicw0ng-hk/HOT-25"
    This reverts commit 9ddae54e92d1a4215954612766f737dbcca85638, reversing
    changes made to 7db4716fc19424bc829220c8f06af9b5e64fd95e.

--- HotZone/cases/urls.py
+++ HotZone/cases/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('caselocation', views.caselocation, name='caselocation'),
     path('caselocation/add', views.caselocation_add, name='caselocation_add'),
-    path('cluster', views.cluster, name='cluster'),
-    path('change_password/', views.change_password, name='change_password'),
-    path('change_password_done/', views.change_password_done, name='change_password_done')
+    path('cluster', views.cluster, name='cluster')
 ]
